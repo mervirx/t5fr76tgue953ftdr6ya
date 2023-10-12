89e766cafdaa02ee5f611fa037c2e9345954ae72
mervirx <136204252+mervirx@users.noreply.github.com>
2023-10-12 23:24:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+<script>
+        function showmessage() {
+            var elementoMessaggio = document.getElementById("showmessage");
+            elementoMessaggio.innerHTML = "showmessage";
+        }
+        var pulsante = document.getElementById("showmessage");
+        pulsante.addEventListener("click", showmessage);
+    </script>
